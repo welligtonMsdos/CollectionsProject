49b71b34022d74d1bbf57b10ba2129d902b1c9a1
welligton silva <welligton_msdos@hotmail.com>
2026-04-23 18:29:45 -0300

    feat: add CollectionApi

---
+++ CollectionApi/Program.cs
@@ -0,0 +1,112 @@
+using CollectionApplication.Interfaces;
+using CollectionApplication.Validators.Concerts;
+using CollectionDomain.Interfaces;
+using CollectionInfrastructure.Data;
+using CollectionInfrastructure.Repositories;
+using CollectionInfrastructure.Service;
+using CollectionShared.Filters;
+using CollectionShared.Middleware;
+using FluentValidation;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Npgsql;
+using Scalar.AspNetCore;
+using System.Data;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("CorsPolicy", builder =>
+    {
+        builder.AllowAnyOrigin()
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
+builder.Services.AddValidatorsFromAssemblyContaining<ConcertCreateValidator>();
+
+builder.Services.Configure<ApiBehaviorOptions>(options =>
+{
+    options.SuppressModelStateInvalidFilter = true;
+});
+
+builder.Services.AddControllers(options =>
+{
+    options.Filters.Add<ValidatorFilter>();
+});
+
+builder.Services.AddOpenApi();
+
+var secret = Environment.GetEnvironmentVariable("JwtSettings__Key");
+
+if (string.IsNullOrEmpty(secret) || secret.Length < 32)
+    throw new InvalidOperationException("JWT key is missing or too short (minimum 32 characters).");
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.RequireHttpsMetadata = false;
+
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+            ValidateIssuer = true,
+            //ValidIssuer = "http://13.59.37.186:5011",
+            ValidIssuer = "http://localhost:5011",
+            ValidateAudience = false,
+            ValidateLifetime = true
+        };
+    });
+
+builder.Services.AddAuthorization();
+
+var connectionString = builder.Configuration.GetConnectionString("CollectionConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+    throw new InvalidOperationException("Connection string 'CollectionConnection' is missing in configuration.");
+
+builder.Services.AddDbContext<CollectionContext>(options => options.UseNpgsql(connectionString));
+
+builder.Services.AddScoped<IDbConnection>(_ => new NpgsqlConnection(connectionString));
+
+builder.Services.AddScoped<IConcertDapperRepository, ConcertDapperRepository>();
+builder.Services.AddScoped<IConcertEFRepository, ConcertEFRepository>();
+builder.Services.AddScoped<IConcertService, ConcertService>();
+
+var app = builder.Build();
+
+app.UseMiddleware<ErrorHandlingMiddleware>();
+
+app.MapOpenApi();
+
+app.MapScalarApiReference(options =>
+{
+    options.Title = "Api Authentication";
+    options.Theme = ScalarTheme.BluePlanet;
+    options.DefaultHttpClient = new(ScalarTarget.JavaScript, ScalarClient.HttpClient);
+    options.CustomCss = "";
+    options.ShowSidebar = true;
+    options.AddPreferredSecuritySchemes("Bearer")
+           .AddHttpAuthentication("Bearer", auth =>
+           {
+               auth.Token = "your-bearer-token";
+           });
+});
+
+app.UseCors("CorsPolicy");
+
+app.UseHttpsRedirection();
+
+app.UseAuthentication();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
